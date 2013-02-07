89683bec668c6d70871c0ef377ff5e3326e5ff8e
Tom Habing <thabing@illinois.edu>
2013-02-06 21:03:17 -0600

    Upgrade to MVC 4.5; Minor code refactoring; Moved function to the IDManager class

--- MetadataUtilities/MetadataFunctions.vb
+++ MetadataUtilities/MetadataFunctions.vb
@@ -6,6 +6,10 @@ Imports System.Security.Permissions
 Imports System.IO
 Imports System.Text
 
+''' <summary>
+''' Collection of misc. shared functions for dealing with metadata
+''' </summary>
+''' <remarks></remarks>
 Public Class MetadataFunctions
 
   ''' <summary>
@@ -32,79 +36,6 @@ Public Class MetadataFunctions
       Return ""
     End If
 
-  End Function
-
-  ''' <summary>
-  ''' Generate a new Medusa Local Identifier which follows this syntax:  HandleProject:Guid-CheckDigit
-  ''' </summary>
-  ''' <returns>Handle</returns>
-  ''' <remarks>HandlePrefix is optional.  A check digit is added to the end of the GUID using the Verhoeff algorithm</remarks>
-  Public Shared Function GenerateLocalIdentifier() As String
-    Dim project As String = MedusaAppSettings.Settings.HandleProject
-    Dim uuid As Guid = Guid.NewGuid
-
-
-    Dim uuidStr As String = uuid.ToString.Replace("-", "")
-    Dim checkD As Char = CheckDigit.GenerateCheckCharacter(uuidStr)
-    Dim handle As String = String.Format("{0}:{1}-{2}", project, uuid.ToString, checkD)
-
-    Return handle
-  End Function
-
-  ''' <summary>
-  ''' Given a complete handle, return just the local part (minus the registered handle prefix)
-  ''' </summary>
-  ''' <param name="handle"></param>
-  ''' <returns></returns>
-  ''' <remarks></remarks>
-  Public Shared Function GetLocalIdentifier(handle As String) As String
-    Dim re As New Regex("^\s*(\d+)/([^:]+:[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}-[A-F0-9](?:\.\d+)?)\s*$", RegexOptions.IgnoreCase)
-    Dim ret As String
-
-    Dim m As Match = re.Match(handle)
-
-    If m.Success Then
-      Dim prefix As String = m.Groups.Item(1).Value
-      Dim project As String = m.Groups.Item(2).Value
-      ret = project
-    Else
-      Throw New Exception("Invalid Handle: " & handle)
-    End If
-
-    Return ret
-  End Function
-
-  ''' <summary>
-  ''' If the given handle is valid return True; else return False.  This checks for valid syntax, prefix, project, and Guid Check Digit.
-  ''' </summary>
-  ''' <param name="handle"></param>
-  ''' <returns></returns>
-  ''' <remarks>HandlePrefix is optional.</remarks>
-  Public Shared Function ValidateHandle(ByVal handle As String) As Boolean
-    Dim re As New Regex("^\s*(?:(\d+)/)?([^:]+):([A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}-[A-F0-9])(?:\.(\d+))?\s*$", RegexOptions.IgnoreCase)
-
-    Dim m As Match = re.Match(handle)
-
-    If m.Success Then
-      Dim prefix As String = m.Groups.Item(1).Value
-      Dim project As String = m.Groups.Item(2).Value
-      Dim uuidPlusCheck As String = m.Groups.Item(3).Value
-      Dim localId As String = m.Groups.Item(4).Value
-
-      If (Not String.IsNullOrWhiteSpace(prefix)) AndAlso prefix <> MedusaAppSettings.Settings.HandlePrefix Then
-        Return False
-      ElseIf project <> MedusaAppSettings.Settings.HandleProject Then
-        Return False
-      ElseIf CheckDigit.ValidateCheckCharacter(uuidPlusCheck.Replace("-", "")) = False Then
-        Return False
-      ElseIf (Not String.IsNullOrWhiteSpace(localId)) AndAlso (Not IsNumeric(localId)) Then
-        Return False
-      End If
-    Else
-      Return False
-    End If
-
-    Return True
   End Function
 
   Public Declare Function FindMimeFromData Lib "urlmon.dll" (ByVal pBC As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> ByVal pwzUrl As String, <MarshalAs(UnmanagedType.LPArray, ArraySubType:=UnmanagedType.I1, SizeParamIndex:=3)> ByVal pBuffer As Byte(), ByVal cbSize As Integer, <MarshalAs(UnmanagedType.LPWStr)> ByVal pwzMimeProposed As String, ByVal dwMimeFlags As Integer, <MarshalAs(UnmanagedType.LPWStr)> ByRef ppwzMimeOut As String, ByVal dwReserved As Integer) As Integer
@@ -209,11 +140,4 @@ Public Class MetadataFunctions
     End If
   End Function
 
-  Public Shared Function GetIdType(id As String) As String
-    Dim idType As String = "LOCAL"
-    If id.StartsWith(MedusaAppSettings.Settings.HandlePrefix & "/") Then
-      idType = "HANDLE"
-    End If
-    Return idType
-  End Function
 End Class
